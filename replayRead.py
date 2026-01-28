6aac88468c80001b5416d0613a6773bc8023b511
s131800 <phippsee3@gmail.com>
2026-01-27 21:33:32 -0500

    Added turn Counter

--- replayRead.py
+++ replayRead.py
@@ -14,7 +14,6 @@ if "clearpoke" in battleLog:
     fullTeams = battleLog[teamsStart+10:teamsEnd-2]
     teamList = fullTeams.split("\n")
     for poke in teamList:
-        print(poke)
         if "p1" in poke:
             pokeStart = poke.index("p1|")
             pokeEnd = poke.index("|", pokeStart+3)
@@ -44,3 +43,30 @@ if "switch" in battleLog:
     firstP2Mon = (battleLog[firstMonP2Index:p2MonEnd]).split("|")
     print(firstP1Mon)
     print(firstP2Mon)
+
+#Start Turn Stuff
+
+oppDictionary = {}
+#have user dictionary to keep up with health basically is the only thing it needs
+userDictionary = {}
+
+if "turn|1" in battleLog:
+    turnStartIndex = battleLog.index("|turn|1")
+    battleTurns = battleLog[turnStartIndex+1:]
+    turnList = battleTurns.split("|turn|")
+
+for turn in turnList:
+    print(turnList)
+    #Keep track of damage values of every pokemon at all times for opponent and user team. These values can easily be passed for math, heuristic, etc.
+    '''Battle Dictionary = {
+        pokemon1 = {
+            health: 100/100,
+            moves: [move1, move2, move3, move4],
+            possibleSets: [set1, set2, set3, ...],
+            ability: [ability],
+            item: [item],
+            }
+    }'''
+
+print(oppDictionary)
+print(userDictionary)
